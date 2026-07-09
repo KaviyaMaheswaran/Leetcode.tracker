d51489af2e641e35f4677e0da638188840c3a9e3
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-07-09 09:15:21 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:15:21 AM
+
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+
+        n = uglyChecker(n, 2);
+        n = uglyChecker(n, 3);
+        n = uglyChecker(n, 5);
+
+        return n == 1;
+    }
+
+    public int uglyChecker(int num, int factor){
+        while(num % factor == 0){
+            num /= factor;
+        }
+
+        return num;
+    }
+}
