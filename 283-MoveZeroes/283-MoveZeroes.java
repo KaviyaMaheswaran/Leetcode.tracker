6c35a4701d858d462c2a30de7f78a51b6e6d7744
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-07-09 09:15:17 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:15:17 AM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0; // Pointer to place the next non-zero element
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap current element with the element at index j 
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;   // Move j to the next index for placing non-zero
+            }
+        }
+    }
+}
