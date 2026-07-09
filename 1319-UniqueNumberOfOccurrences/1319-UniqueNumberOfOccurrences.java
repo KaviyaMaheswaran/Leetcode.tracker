81dba85d55af86574f767aa7e9481af6b417f4b2
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-07-09 09:14:10 +0530

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:14:09 AM
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Arrays.sort(arr);
+        Set<Integer> st = new HashSet<>();
+        int i = 0 , cnt = 0;
+
+        while(i < arr.length) {
+            int j = i;
+            while(j + 1 < arr.length && arr[j + 1] == arr[j]) {
+                j++;
+            }
+            int freq = j - i + 1;
+            st.add(freq);
+            cnt++;
+            i = j + 1;
+        }
+        return st.size() == cnt;
+    }
+}
