51af149cffd0efadf92ce9958e00d55398e09ac2
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-02 09:38:43 +0530

    Create 3705-FindTheLargestAlmostMissingInteger

---
+++ 3705-FindTheLargestAlmostMissingInteger/3705-FindTheLargestAlmostMissingInteger.java
@@ -0,0 +1,23 @@
+// Last updated: 9/2/2026, 9:38:43 AM
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int[] count = new int[51];
+        for (int i = 0; i <= nums.length - k; i++) {
+            boolean[] seen = new boolean[51];
+            for (int j = i; j < i + k; j++) {
+                seen[nums[j]] = true;
+            }
+            for (int x = 0; x <= 50; x++) {
+                if (seen[x]) {
+                    count[x]++;
+                }
+            }
+        }
+        for (int x = 50; x >= 0; x--) {
+            if (count[x] == 1) {
+                return x;
+            }
+        }
+        return -1;
+    }
+}
