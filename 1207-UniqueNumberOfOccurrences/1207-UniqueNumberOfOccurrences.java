a0e0575de2bbf23573ca5bfe91d9330c79095097
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-07 16:19:13 +0530

    Create 1207-UniqueNumberOfOccurrences

---
+++ 1207-UniqueNumberOfOccurrences/1207-UniqueNumberOfOccurrences.java
@@ -0,0 +1,20 @@
+// Last updated: 9/7/2026, 4:19:17 PM
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Arrays.sort(arr);
+        Set<Integer> st = new HashSet<>();
+        int i = 0 , cnt = 0;
+
+        while(i < arr.length) {
+            int j = i;
+            while(j + 1 < arr.length && arr[j + 1] == arr[j]) {
+                j++;
+            }
+            int freq = j - i + 1;
+            st.add(freq);
+            cnt++;
+            i = j + 1;
+        }
+        return st.size() == cnt;
+    }
+}
