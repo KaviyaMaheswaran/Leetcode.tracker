011c41416aec7a8d274c7efb724e091f469bc5c9
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-07 16:18:43 +0530

    Create 2200-FindAllKDistantIndicesInAnArray

---
+++ 2200-FindAllKDistantIndicesInAnArray/2200-FindAllKDistantIndicesInAnArray.java
@@ -0,0 +1,22 @@
+// Last updated: 9/7/2026, 4:18:46 PM
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == key) {
+                int start = Math.max(0, i - k);
+                int end = Math.min(nums.length - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    visited[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
