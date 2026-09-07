9903167bccf7752b1387f819ca193214ce15f69b
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-07 16:19:20 +0530

    Create 1154-DayOfTheYear

---
+++ 1154-DayOfTheYear/1154-DayOfTheYear.java
@@ -0,0 +1,20 @@
+// Last updated: 9/7/2026, 4:19:24 PM
+class Solution {
+    public int dayOfYear(String date) {
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+        int[] days= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            days[1] = 29;
+        }
+        int count = 0;
+        for (int i = 0; i < month - 1; i++) {
+            count+= days[i];
+        }
+        count += day;
+
+        return count;
+    }
+}
