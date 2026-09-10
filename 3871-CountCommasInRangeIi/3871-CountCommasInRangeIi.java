7f00a9c7b603e910786c4dbb91fd0aedb14355d2
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-10 17:27:19 +0530

    Create 3871-CountCommasInRangeIi

---
+++ 3871-CountCommasInRangeIi/3871-CountCommasInRangeIi.java
@@ -0,0 +1,12 @@
+// Last updated: 9/10/2026, 5:27:19 PM
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long x = 1000;
+        while (x <= n) {
+            ans += n - x + 1;
+            x *= 1000;
+        }
+        return ans;
+    }
+}
