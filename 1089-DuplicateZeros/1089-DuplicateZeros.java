75efdccbf2fd06bd483c0d8c57628628dd40b3fc
KaviyaMaheswaran <kaviyamaheswaran9@gmail.com>
2026-09-10 14:56:34 +0530

    Create 1089-DuplicateZeros

---
+++ 1089-DuplicateZeros/1089-DuplicateZeros.java
@@ -0,0 +1,13 @@
+// Last updated: 9/10/2026, 2:56:34 PM
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 1; j > i; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                i++;
+            }
+        }
+    }
+}
